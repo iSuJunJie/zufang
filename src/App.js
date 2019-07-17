c520dec042d76d56d047498f845e6450ac74a805
SuJunJie <864273312@qq.com>
2019-07-17 10:20:37 +0800

    项目粗俗化

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {HashRouter,Route,Link,Switch,Redirect} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import Home from './components/home/Home.jsx'
+import Login from './components/login/Login.jsx'
+class App extends React.Component {
+
+  render(){
+    return(
+      <HashRouter>
+        {/* 配置路由规则 */}
+        <Switch>
+           <Redirect exact path="/" to="/home"></Redirect>
+           <Route path="/home" component={Home}/>
+           <Route path="/login" component={Login}/>
+        </Switch>
+      </HashRouter>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Login extends React.Component {
+
+  render(){
+    return(
+      <div>
+        Login组件
+      </div>
+    )
+  }
+}
+
+export default Login
